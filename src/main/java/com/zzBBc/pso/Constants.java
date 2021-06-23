e7cd43aa595d1db9de7109dd4190c14e4ec57fe7
zzBBc <zzbbc123@gmail.com>
2021-06-24 04:01:11 +0700

    Update constant for both pso and branchbound

--- src/main/java/com/zzBBc/pso/Constants.java
+++ src/main/java/com/zzBBc/pso/Constants.java
@@ -4,8 +4,10 @@ public final class Constants {
 	public static final double W = 0.7;
 	public static final double C1 = 2;
 	public static final double C2 = 2;
-	public static final int MAX_EPOCHS = 20000;
-	public static final int MAX_STATIC_EPOCHS = 250;
-	public static final int SWARM_SIZE = 100;
-	public static final int MAX_INFORMERS = 5;
+	public static final int MAX_EPOCHS = 2000;
+	public static final int SWARM_SIZE = 10;
+
+	public static final int CITY_COUNT = 13;
+	public static final boolean PRINT_DISTANCE_IMPROVED = false;
+	public static final String FILE_PATH = "graph_city.txt";
 }
